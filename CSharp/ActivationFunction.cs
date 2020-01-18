f9dffcc13beb95f2c5054af65744391cb89cf6fb
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-01-19 01:44:55 +0200

    add c# version

---
+++ CSharp/ActivationFunction.cs
@@ -0,0 +1,26 @@
+using static Synapses.Model.NetElems.Activation;
+
+namespace SynapsesCs
+{
+    public class ActivationFunction
+    {
+        Function contents;
+
+        public ActivationFunction(Function _contents)
+        {
+            contents = _contents;
+        }
+
+        public static ActivationFunction sigmoid =
+            new ActivationFunction(ActivationFunctionModule.sigmoid);
+
+        public static ActivationFunction identity =
+            new ActivationFunction(ActivationFunctionModule.identity);
+
+        public static ActivationFunction tanh =
+            new ActivationFunction(ActivationFunctionModule.tanh);
+
+        public static ActivationFunction leakyReLU =
+            new ActivationFunction(ActivationFunctionModule.leakyReLU);
+    }
+}
