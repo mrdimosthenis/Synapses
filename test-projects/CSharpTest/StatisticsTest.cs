2dd6e3b5d4330cad4a43063afaeeede4b012b19f
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-01-19 20:32:08 +0200

    complete c# tests

---
+++ test-projects/CSharpTest/StatisticsTest.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Xunit;
+using SynapsesCs;
+
+namespace CSharpTest
+{
+    public class StatisticsTest
+    {
+        static IEnumerable<(double[], double[])> expectedWithOutputValues =
+            new List<(double[], double[])>()
+            {
+                (new double[] {0.0, 0.0, 1.0}, new double[] {0.0, 0.0, 1.0}),
+                (new double[] {0.0, 0.0, 1.0}, new double[] {0.0, 1.0, 1.0})
+            };
+
+        [Fact]
+        public void rootMeanSquareError()
+        {
+            Assert.Equal(
+                0.7071067811865476,
+                Statistics.rootMeanSquareError(expectedWithOutputValues)
+            );
+        }
+    }
+}
