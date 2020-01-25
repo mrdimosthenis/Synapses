769b0bd55c8c89b717739b6702907562dbac251e
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-01-25 23:03:55 +0200

    fix network working

--- implementation/Python/model/net_elems/network.py
+++ implementation/Python/model/net_elems/network.py
@@ -19,21 +19,19 @@ def init(layer_sizes: Sequence,
     return layer_sizes \
         .zip(layer_sizes.tail()) \
         .zip_with_index() \
-        .map(
-        lambda t: layer.init(
-            t[0][0],
-            t[0][1],
-            activation_f(t[1]),
-            lambda: lambda: weight_init_f(t[1])
-        )
-    )
+        .map(lambda t:
+             layer.init(
+                 t[0][0],
+                 t[0][1],
+                 activation_f(t[1]),
+                 lambda: lambda: weight_init_f(t[1])))
 
 
-def output(input: Sequence,
+def output(input_val: Sequence,
            network_val: Network
            ) -> Sequence:
     return network_val.fold_left(
-        input,
+        input_val,
         lambda acc, x: layer.output(acc, x)
     )
 
@@ -41,16 +39,16 @@ def output(input: Sequence,
 def fed_forward_acc_f(already_fed: Sequence,
                       next_layer: Layer
                       ) -> Sequence:
-    (errors, layer_val) = already_fed.head()
-    next_input = layer.output(errors, layer_val)
-    return utilities\
+    (errors_val, layer_val) = already_fed.head()
+    next_input = layer.output(errors_val, layer_val)
+    return utilities \
         .lazy_cons((next_input, next_layer), already_fed)
 
 
-def fed_forward(input: Sequence,
+def fed_forward(input_val: Sequence,
                 network: Network
                 ) -> Sequence:
-    init_feed = seq([(input, network.head())])
+    init_feed = seq([(input_val, network.head())])
     return network \
         .tail() \
         .fold_left(init_feed, lambda acc, x: fed_forward_acc_f(acc, x))
@@ -60,20 +58,20 @@ def back_propagated_acc_f(learning_rate: float,
                           errors_with_already_propagated: Sequence,
                           input_with_layer: (Sequence, Layer)
                           ) -> (Sequence, Sequence):
-    (errors, already_propagated) = errors_with_already_propagated
+    (errors_val, already_propagated) = errors_with_already_propagated
     (last_input, last_layer) = input_with_layer
     last_output_with_errors = layer \
         .output(last_input, last_layer) \
-        .zip(errors)
+        .zip(errors_val)
     (next_errors, propagated_layer) = layer.back_propagated(
         learning_rate,
         last_input,
         last_output_with_errors,
         last_layer
     )
-    next_already_propagated = utilities\
+    next_already_propagated = utilities \
         .lazy_cons(propagated_layer, already_propagated)
-    return (next_errors, next_already_propagated)
+    return next_errors, next_already_propagated
 
 
 def back_propagated(learning_rate: float,
@@ -81,11 +79,11 @@ def back_propagated(learning_rate: float,
                     reversed_inputs_with_layers: Sequence
                     ) -> (Sequence, Network):
     (last_input, last_layer) = reversed_inputs_with_layers.head()
-    output = layer.output(last_input, last_layer)
-    errors = output \
+    output_val = layer.output(last_input, last_layer)
+    errors_val = output_val \
         .zip(expected_output) \
         .map(lambda t: t[0] - t[1])
-    output_with_errors = output.zip(errors)
+    output_with_errors = output_val.zip(errors_val)
     (init_errors, first_propagated) = layer.back_propagated(
         learning_rate,
         last_input,
@@ -101,38 +99,38 @@ def back_propagated(learning_rate: float,
 
 
 def errors_with_fit_net(learning_rate: float,
-                        input: Sequence,
+                        input_val: Sequence,
                         expected_output: Sequence,
                         network: Network
                         ) -> (Sequence, Network):
     return back_propagated(
         learning_rate,
         expected_output,
-        fed_forward(input, network)
+        fed_forward(input_val, network)
     )
 
 
 def errors(learning_rate: float,
-           input: Sequence,
+           input_val: Sequence,
            expected_output: Sequence,
            network: Network
            ) -> Sequence:
     return errors_with_fit_net(
         learning_rate,
-        input,
+        input_val,
         expected_output,
         network
     )[0]
 
 
 def fit(learning_rate: float,
-        input: Sequence,
+        input_val: Sequence,
         expected_output: Sequence,
         network: Network
         ) -> Sequence:
     return errors_with_fit_net(
         learning_rate,
-        input,
+        input_val,
         expected_output,
         network
     )[1]
