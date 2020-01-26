13cec923cb0bb89cf89dc9bda837d4eb8f99fe9d
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-01-27 00:04:02 +0200

    add setup and licence

--- implementation/Python/setup.py
+++ implementation/Python/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="SynapsesPy",
+    version="0.0.1alpha",
+    author="Dimosthenis Michailidis",
+    author_email="mrdimosthenis@hotmail.com",
+    description="A lightweight Neural Network library, for js, jvm and .net",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/mrdimosthenis/Synapses",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
