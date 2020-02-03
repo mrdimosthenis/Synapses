8ed3978b66f4c654c0751ee11f31701105d932e5
mrdimosthenis <mrdimosthenis@hotmail.com>
2020-02-03 12:06:00 +0200

    fix build script

--- bash-scripts/build-libs.sh
+++ bash-scripts/build-libs.sh
@@ -1,45 +1,56 @@
 cd ../
 cd implementation
 
-cd FSharp
+# F#
+cd DotNet/Synapses
 rm -r bin
 rm -r obj
 rm README.md
+cp ../../../README.md README.md
 dotnet restore
 dotnet build --configuration Release
 dotnet pack --configuration Release
-cp ../../README.md README.md
-cd ../
+cd ../../
 
-cd CSharp
+# C#
+cd DotNet/SynapsesCSharp
 rm -r bin
 rm -r obj
 rm README.md
+cp ../../../README.md README.md
 dotnet restore
 dotnet build --configuration Release
 dotnet pack --configuration Release
-cp ../../README.md README.md
-cd ../
+cd ../../
 
 cd Scala
+rm -r project/target
 rm -r target
 rm README.md
-sbt assembly
 cp ../../README.md README.md
+sbt assembly
 cd ../
 
 cd ScalaJS
+rm -r project/target
 rm -r target
 rm README.md
-sbt fullOptJS
 cp ../../README.md README.md
+sbt fullOptJS
 cd ../
 
 cd JavaScript
 rm -r target
 rm synapses-opt.js
 rm README.md
-cp ../ScalaJS/target/scala-2.13/synapses-opt.js synapses-opt.js
 cp ../../README.md README.md
+cp ../ScalaJS/target/scala-2.13/synapses-opt.js synapses-opt.js
 npm build
+cd ../
+
+cd Python
+rm -r dist
+rm README.md
+cp ../../README.md README.md
+poetry build
 cd ../
