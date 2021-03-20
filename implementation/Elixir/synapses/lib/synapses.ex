a295ec5a6c1f8aedc03c38421cd4a0fb1c81136a
deploy.sh <>
2021-03-20 12:43:07 +0200

    implement uncons

--- implementation/Elixir/synapses/lib/synapses.ex
+++ implementation/Elixir/synapses/lib/synapses.ex
@@ -1,18 +1,18 @@
 defmodule Synapses do
-  @moduledoc """
-  Documentation for Synapses.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Synapses.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defmodule NeuralNetwork do
+    def init(layers) do
+      :gleam_synapses@neural_network.init(layers)
+      layers
+    end
   end
+
+  def uncons(stream) do
+    hd = Stream.take(stream, 1)
+         |> Enum.at(0)
+    tl = Stream.drop(stream, 1)
+    {hd, tl}
+  end
+
 end
--- implementation/Elixir/synapses/test/synapses_test.exs
+++ implementation/Elixir/synapses/test/synapses_test.exs
@@ -1,8 +1,29 @@
 defmodule SynapsesTest do
   use ExUnit.Case
+  import Synapses
+  alias Synapses.{NeuralNetwork}
   doctest Synapses
 
-  test "greets the world" do
-    assert Synapses.hello() == :world
+  test "uncons" do
+    infinite_stream = Stream.iterate(0, &(&1 + 1))
+    singleton_stream = infinite_stream |> Stream.take(1)
+    empty_stream = infinite_stream |> Stream.take(0)
+
+    {a, tl1} = uncons(infinite_stream)
+    {b, _} = uncons(tl1)
+    {c, tl2} = uncons(singleton_stream)
+    {d, tl3} = uncons(empty_stream)
+
+    assert a == 0
+    assert b == 1
+    assert c == 0
+    assert Enum.empty?(tl2)
+    assert d == nil
+    assert Enum.empty?(tl3)
   end
+
+  test "nn" do
+    assert NeuralNetwork.init([3, 2, 2]) == [3, 2, 2]
+  end
+
 end
