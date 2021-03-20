0441de09ed46432e4dda90b2d7b559a763a9241c
deploy.sh <>
2021-03-20 09:50:31 +0200

    create elixir project

---
+++ implementation/Elixir/synapses/lib/synapses.ex
@@ -0,0 +1,18 @@
+defmodule Synapses do
+  @moduledoc """
+  Documentation for Synapses.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Synapses.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ implementation/Elixir/synapses/test/synapses_test.exs
@@ -0,0 +1,8 @@
+defmodule SynapsesTest do
+  use ExUnit.Case
+  doctest Synapses
+
+  test "greets the world" do
+    assert Synapses.hello() == :world
+  end
+end
