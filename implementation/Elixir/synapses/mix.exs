11c1d9b0b0ed076999e772967ea14e92a7e5fe90
deploy.sh <>
2021-03-21 20:59:52 +0200

    prepare mix.exs file for release

--- implementation/Elixir/synapses/mix.exs
+++ implementation/Elixir/synapses/mix.exs
@@ -4,10 +4,13 @@ defmodule Synapses.MixProject do
   def project do
     [
       app: :synapses,
-      version: "0.1.0",
+      version: "7.3.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      description: "A neural network library for the Elixir language.",
+      package: package()
     ]
   end
 
@@ -26,6 +29,20 @@ defmodule Synapses.MixProject do
       {:gleam_stdlib, "~> 0.14.0"},
       {:gleam_decode, "~> 1.7.0", override: true},
       {:gleam_synapses, "~> 0.0.1"}
+    ]
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      # name: "emel",
+      # These are the default files included in the package
+      # files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+      #           license* CHANGELOG* changelog* src),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/mrdimosthenis/Synapses"
+      }
     ]
   end
 end
